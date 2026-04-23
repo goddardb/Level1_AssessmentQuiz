cbf7a7573dd432df3ba144f06216e1a34a8436a3
goddardb <goddardb@stu.otc.school.nz>
2026-04-24 09:59:18 +1200

    docstring and intialising

---
+++ NCEA Assessment Mauao Quiz.py
@@ -0,0 +1,10 @@
+'''This is my sumbission for NCEA Level 1 Assessment 92004.
+It is a quiz based on the Maori korero of the legend of Mauao, and users will have to answer eight multiple choice questions, 
+recieving a score at the end. Created by Ben Goddard 24/04/2026 till //2026.'''
+
+#Initialising constants and importing.
+import time
+import os
+NUM_OF_QUES = 8
+POINTS_PER_ANS = 1
+score = 0
